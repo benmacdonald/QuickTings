f9fe7a96c62ee321aa450db597ecd13d4953ea50
tomalama <tommy.nguyen7@hotmail.com>
2017-06-10 00:53:20 -0400

    Fixed Your Orders navigation

--- app/src/main/java/com/uottawa/benjaminmacdonald/quicktings/Activities/YourOrdersActivity.java
+++ app/src/main/java/com/uottawa/benjaminmacdonald/quicktings/Activities/YourOrdersActivity.java
@@ -11,5 +11,15 @@ public class YourOrdersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_your_orders);
+
+        //Add the back button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    //Back button functionality
+    @Override
+    public boolean onSupportNavigateUp(){
+        finish();
+        return true;
     }
 }
