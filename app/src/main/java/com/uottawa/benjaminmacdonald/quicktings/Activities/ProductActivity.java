f666d0b5cf3b5e5937d72917ed014ac8efa660c9
tomalama <tommy.nguyen7@hotmail.com>
2017-06-28 18:16:57 -0400

    Product activity FAB turns into toolbar

--- app/src/main/java/com/uottawa/benjaminmacdonald/quicktings/Activities/ProductActivity.java
+++ app/src/main/java/com/uottawa/benjaminmacdonald/quicktings/Activities/ProductActivity.java
@@ -4,14 +4,19 @@ import android.content.Intent;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.github.fafaldo.fabtoolbar.widget.FABToolbarLayout;
 import com.uottawa.benjaminmacdonald.quicktings.Classes.ProductItem;
 import com.uottawa.benjaminmacdonald.quicktings.R;
 
 public class ProductActivity extends AppCompatActivity {
+
+    private FABToolbarLayout toolbarLayout;
 
     String description;
     String details;
@@ -21,12 +26,35 @@ public class ProductActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
 
+        toolbarLayout = (FABToolbarLayout) findViewById(R.id.fabtoolbar);
+        View fab = findViewById(R.id.fabtoolbar_fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toolbarLayout.show();
+            }
+        });
+
+
+        View scrollView = findViewById(R.id.contentView);
+        scrollView.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
+            @Override
+            public void onScrollChanged() {
+                toolbarLayout.hide();
+            }
+        });
+
         Intent intent = getIntent();
         ProductItem productItem = (ProductItem) intent.getExtras().getParcelable("PRODUCT");
 
         setupProduct(productItem);
     }
 
+    @Override
+    public void onBackPressed() {
+        toolbarLayout.hide();
+    }
 
     private void setupProduct(ProductItem productItem) {
 
@@ -48,7 +76,8 @@ public class ProductActivity extends AppCompatActivity {
         description = productItem.getDescription();
         details = productItem.getOrigin();
 
-        TextView textToChange = (TextView) findViewById(R.id.textToChange);
-        textToChange.setText(productItem.getDescription());
+        //TODO: Description returning null
+//        TextView textToChange = (TextView) findViewById(R.id.textToChange);
+//        textToChange.setText(productItem.getDescription());
     }
 }
