1d737d778f9e116a1fec03738ac2b360ed9c9353
tomalama <tommy.nguyen7@hotmail.com>
2017-06-29 11:14:51 -0400

    Made FAB toolbar more FABulous

--- app/src/main/java/com/uottawa/benjaminmacdonald/quicktings/Activities/ProductActivity.java
+++ app/src/main/java/com/uottawa/benjaminmacdonald/quicktings/Activities/ProductActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewTreeObserver;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -26,9 +27,12 @@ public class ProductActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
 
+        //Add the back button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Adds onClickListener to FAB
         toolbarLayout = (FABToolbarLayout) findViewById(R.id.fabtoolbar);
         View fab = findViewById(R.id.fabtoolbar_fab);
-
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,11 +40,19 @@ public class ProductActivity extends AppCompatActivity {
             }
         });
 
-
+        //Closes fabtoolbar if user scrolls
         View scrollView = findViewById(R.id.contentView);
         scrollView.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
             @Override
             public void onScrollChanged() {
+                toolbarLayout.hide();
+            }
+        });
+
+        //Cancel button
+        Button cancelButton = (Button) findViewById(R.id.cancelButton);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick (View v) {
                 toolbarLayout.hide();
             }
         });
@@ -53,7 +65,14 @@ public class ProductActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        toolbarLayout.hide();
+
+        //Checks if the FAB is the button or the toolbar
+        if (toolbarLayout.isFab()) {
+            finish();
+        } else {
+            toolbarLayout.hide();
+        }
+
     }
 
     private void setupProduct(ProductItem productItem) {
@@ -61,8 +80,11 @@ public class ProductActivity extends AppCompatActivity {
         ImageView productImage = (ImageView) findViewById(R.id.productImage);
         Glide.with(this).load(productItem.getImageUrl()).into(productImage);
 
-        TextView titleView = (TextView) findViewById(R.id.titleLabel);
-        titleView.setText(productItem.getName());
+        TextView nameView = (TextView) findViewById(R.id.nameLabel);
+        nameView.setText(productItem.getName());
+
+        TextView nameView2 = (TextView) findViewById(R.id.nameLabel2);
+        nameView2.setText(productItem.getName());
 
         TextView catView = (TextView) findViewById(R.id.catLabel);
         catView.setText(productItem.getCategory());
@@ -70,14 +92,28 @@ public class ProductActivity extends AppCompatActivity {
         TextView priceView = (TextView) findViewById(R.id.priceLabel);
         priceView.setText("$"+ productItem.getPrice() / 100.00);
 
+        TextView priceView2 = (TextView) findViewById(R.id.priceLabel2);
+        priceView2.setText("$"+ productItem.getPrice() / 100.00);
+
         TextView volumeView = (TextView) findViewById(R.id.volumeLabel);
         volumeView.setText(productItem.getVolume() + " mL " +productItem.getUnitType());
 
         description = productItem.getDescription();
         details = productItem.getOrigin();
 
-        //TODO: Description returning null
-//        TextView textToChange = (TextView) findViewById(R.id.textToChange);
-//        textToChange.setText(productItem.getDescription());
+        TextView textToChange = (TextView) findViewById(R.id.textToChange);
+        if (productItem.getDescription() == null) {
+            //TODO: If description returns null, do something
+        } else {
+            textToChange.setText(productItem.getDescription());
+        }
+
+    }
+
+    //ActionBar back button functionality
+    @Override
+    public boolean onSupportNavigateUp(){
+        finish();
+        return true;
     }
 }
